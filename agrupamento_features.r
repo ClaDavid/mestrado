5b006f5e094de234e13492fff0f886332c0bb5ea
Clarissa <clarissa.david@semantix.com.br>
2019-05-04 20:14:41 -0300

    add silhouette test

--- agrupamento_features.r
+++ agrupamento_features.r
@@ -1,6 +1,8 @@
 library(e1071)
 library(data.table)
+library(zoo)
 library(ppclust)
+library(fclust)
 
 
 tav <- fread(file = "reuters_pos_tag_final.csv", header = TRUE, check.names = FALSE)
@@ -22,7 +24,7 @@ for( index in 1:5  )
   for( i in 2:floor( sqrt( nrow( t(tav) ) ) ) )
   {
     best_start <- .Machine$integer.max
-    for( index_best_start in 1:10 )
+    for( index_best_start in 1:100 )
     {
       fuzzyFeature <- cmeans(t(tav), i)
       if( best_start > fuzzyFeature$withinerror )
@@ -38,8 +40,8 @@ for( index in 1:5  )
     d = rbind(d, data.frame(i, result_index))
   }
   
-  write.csv(d, file = paste(paste('git//mestrado//fukuyama//fukuyama_sugeno_teste', index, sep ="_" ), 'csv',sep="."), row.names = FALSE)
-  png(filename = paste(paste("git//mestrado//fukuyama//fukuyama_sugeno_teste", index, sep = "_"), 'png', sep = "." ) )
+  write.csv(d, file = paste(paste('/home/semantix/Documentos/mestrado/mestrado/fukuyama/fukuyama_sugeno_teste', index, sep ="_" ), 'csv',sep="."), row.names = FALSE)
+  png(filename = paste(paste("/home/semantix/Documentos/mestrado/mestrado/fukuyama/fukuyama_sugeno_teste", index, sep = "_"), 'png', sep = "." ) )
   plot(d$i, d$result_index, type = "o")
   dev.off()
 
@@ -66,16 +68,53 @@ for( index in 1:10  )
 }
 
 #####joelho
-  knee = c()
-  for( i in 2:floor( sqrt( nrow( t(tav) ) ) ) )
-  {
-    fuzzyFeature <- cmeans(t(tav), i)
-    knee <- c(knee, fuzzyFeature$withinerror)
-  }
+for(index in 1:10){
+knee = c()
+
+for( i in 2:floor( sqrt( nrow( t(tav) ) ) ) )
+{
+  fuzzyFeature <- cmeans(t(tav), i)
+  knee <- c(knee, fuzzyFeature$withinerror)
+  
+}
+delta_one_element = diff(knee)
+soma_deltas = rollapply(delta_one_element, 2, sum)
+cluster_utilizado = which.min(delta_one_element) + 2
 cluster_knee = c(2:floor( sqrt( nrow( t(tav) ) ) ))
-png(filename = "git//mestrado//joelho//joelho_teste.png")
-plot(cluster_knee, knee, type = "o", xlab="erro",ylab="cluster")
+png(filename = paste(paste("/home/semantix/Documentos/mestrado/mestrado/joelho/joelho_teste", index, sep = "_"), 'png', sep = "." ) )
+plot(cluster_knee, knee, type = "o", xlab="cluster",ylab="erro")
 dev.off()
+print(cluster_utilizado)
+}
+
+#####silhueta
+
+start.time <- Sys.time()
+
+d = NULL
+fuzzyFeature = list()
+for( i in 2:floor( sqrt( nrow( t(tav) ) ) ) )
+{
+  fuzzyClustering <- cmeans(t(tav), i)
+  fuzzyFeature[[i]] <- fuzzyClustering$membership
+}
+
+for( i in 2:floor( sqrt( nrow( t(tav) ) ) ) )
+{
+  result_index <- SIL.F(t(tav), fuzzyFeature[[i]], alpha = 1)
+  d = rbind(d, data.frame(i, result_index))
+}
+
+
+write.csv(d, file = '/home/semantix/Documentos/mestrado/mestrado/silhueta/silhueta_teste.csv', row.names = FALSE)
+png(filename = "/home/semantix/Documentos/mestrado/mestrado/silhueta/silhueta_teste.png" )
+plot(d$i, d$result_index, type = "o")
+dev.off()
+
+end.time <- Sys.time()
+
+time.taken <- end.time - start.time
+
 
 # the ideal number of cluster is 5
 
