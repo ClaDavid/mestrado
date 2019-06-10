0a98b10d8263835918749b77da4a8f76028210fd
Clarissa <clarissa.david@semantix.com.br>
2019-06-09 22:16:36 -0300

    add silhouette as fitness to pso

--- agrupamento_docs.r
+++ agrupamento_docs.r
@@ -10,13 +10,22 @@ doc_previo <- cmeans(tav, 4)
 
 doc_fcm <- fcm.build(as.matrix(tav), t(doc_previo$membership))
 
+
+rodei agrupamento -> cada particula tem numero elementos = numero de grupos -> pega essa particula -> e ai roda uma tecnica de classificacao baseada em distancia
+
+
+-----> silhueta, rand ou jaccard
 runTecAcuracia <- function(w=rep(1,nrow(dataset)), dataset, u){
-  acc = c()
+  silhouette = c()
   for(i in 1:10){
-    doc_fcm <- fcm.build(as.matrix(tav), t(doc_previo$membership), w)
-    uf<-c(); for(i in 1:nrow(ftotal)) uf<-rbind(uf,colSums(matrix(ssc$u[as.logical(ssc$matM[i,]),],sum(ssc$matM[i,]),ncol(ssc$u)))) 
-    acc<-c(acc,sum(apply(uf,2,which.max)==labels)/length(labels)*100)
+    for( i in 2:floor( sqrt( nrow( tav ) ) ) ){
+      doc_fcm <- fcm.build(as.matrix(tav), t(doc_previo$membership), w)
+      
+    }
+    
   }
   #u.plot(ssc$u)
-  return(mean(acc))
+  return(mean(silhouette))
 }
+
+------> pra comparar, fazer o mesmo processo só que com a distancia euclidiana normal
