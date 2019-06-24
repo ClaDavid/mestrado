a02a6d89b718ab412db1dacffff9875da6ccc15e
Clarissa David <clarissa.david05@gmail.com>
2019-06-23 21:37:52 -0300

    add mean of silhouette in runTecAcuracia

--- agrupamento_docs.r
+++ agrupamento_docs.r
@@ -1,31 +1,29 @@
 library("e1071")
 library("pso")
-library("BBmisc")
 library(data.table)
-source("git/mestrado/fcm.r")
+library(fclust)
+source("fcm.r")
 
 tav <- fread(file = "reuters_pos_tag_final.csv", header = TRUE, check.names = FALSE)
 
-doc_previo <- cmeans(tav, 4)
 
-doc_fcm <- fcm.build(as.matrix(tav), t(doc_previo$membership))
+#rodei agrupamento -> cada particula tem numero elementos = numero de grupos -> pega essa particula -> e ai roda uma tecnica de classificacao baseada em distancia
 
-
-rodei agrupamento -> cada particula tem numero elementos = numero de grupos -> pega essa particula -> e ai roda uma tecnica de classificacao baseada em distancia
-
-
------> silhueta, rand ou jaccard
-runTecAcuracia <- function(w=rep(1,nrow(dataset)), dataset, u){
+#-----> silhueta, rand ou jaccard
+runTecAcuracia <- function(w=rep(1,nrow(dataset)), dataset){
   silhouette = c()
   for(i in 1:10){
-    for( i in 2:floor( sqrt( nrow( tav ) ) ) ){
-      doc_fcm <- fcm.build(as.matrix(tav), t(doc_previo$membership), w)
-      
+    for( i in 2:floor( sqrt( nrow( dataset ) ) ) ){
+      doc_previo <- cmeans(dataset, i)
+      doc_fcm <- fcm.build(as.matrix(dataset), t(doc_previo$membership), w)
+      silhouette = cbind(silhouette, SIL.F(Xca = dataset, U = t(doc_fcm$u), alpha = 0))
+      print(silhouette)
     }
-    
   }
   #u.plot(ssc$u)
   return(mean(silhouette))
 }
 
-------> pra comparar, fazer o mesmo processo só que com a distancia euclidiana normal
+#ps perguntar porque alpha tem que ser 0
+
+#------> pra comparar, fazer o mesmo processo só que com a distancia euclidiana normal
