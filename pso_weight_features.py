65ac7f8705dc2a2e6014187f90a814553164319c
Clarissa David <clarissa.david05@gmail.com>
2019-08-28 08:26:38 -0300

    bug fix transform cost_hist list of floats to str

--- pso_weight_features.py
+++ pso_weight_features.py
@@ -7,7 +7,7 @@ options = {'c1': 1.49618, 'c2': 2, 'w':0.9}
 optimizer = ps.single.GlobalBestPSO(n_particles=100, dimensions=1774, options=options)
 best_cost, best_pos = optimizer.optimize(running_clust.runOtimizaPesoAtributo, iters=100)
 cost_hist = optimizer.cost_history
-cost_hist_string = cost_hist.astype(str)
+cost_hist_string = list( map(str, cost_hist) )
 np.savetxt("cost_hist.csv", cost_hist_string, delimiter=",", fmt=('%s'))
 best_pos_string = best_pos.astype(str)
 np.savetxt("best_pos_par.csv", best_pos_string, delimiter=",", fmt=('%s'))
